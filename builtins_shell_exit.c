b4d181914dfd4991f109e59d32f570d00505ebbc
Nidal-h <hadrou.nidal@gmail.com>
2023-11-13 13:22:57 -0800

    0x16. C - Simple Shell

---
+++ builtins_shell_exit.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * shell_exit : Terminates the shell.
+ * @arguments: The array of Arguments.
+ */
+void shell_exit(char **arguments)
+{
+	int s = 0;
+
+	if (arguments[1] != NULL)
+	{
+		s = _atoi(arguments[1]);
+	}
+	free_tokens(arguments);
+	free_last_input();
+	exit(s);
+}
