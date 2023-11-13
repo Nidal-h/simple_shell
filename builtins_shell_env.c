8650700c1b90d84a1fe0de9bc8f0dcaf880f13a2
Nidal-h <hadrou.nidal@gmail.com>
2023-11-13 13:15:57 -0800

    0x16. C - Simple Shell

---
+++ builtins_shell_env.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * shell_env : Displays all variables of the environment.
+ * Return: Void.
+ */
+int shell_env(void)
+{
+	int j;
+
+	for (j = 0; environ[j]; j++)
+	{
+		_puts(environ[j]);
+		_putchar('\n');
+	}
+
+	return (0);
+}
