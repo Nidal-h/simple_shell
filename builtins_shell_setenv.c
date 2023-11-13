11f59b2ee0438c33cec533cf018d703edbe9d9b4
Nidal-h <hadrou.nidal@gmail.com>
2023-11-13 13:34:20 -0800

    0x16. C - Simple Shell

---
+++ builtins_shell_setenv.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * shell_setenv : Assigns a value to an environment variable.
+ * @arguments: Arguments specifies the name and value of the environment variable.
+ */
+int shell_setenv(char **arguments)
+{
+	char *v, *n;
+
+	if (arguments[1] == NULL || arguments[2] == NULL)
+	{
+		_puterror("Usage: setenv VARIABLE VALUE\n");
+		return (-1);
+	}
+
+	n = arguments[1];
+	v = arguments[2];
+
+	if (setenv(n, v, 1) != 0)
+	{
+		_puterror("setenv");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * shell_unsetenv : Removes a specified environment variable.
+ * @arguments: Arguments specifies the name of the environment variable.
+ */
+int shell_unsetenv(char **arguments)
+{
+	char *n;
+
+	if (arguments[1] == NULL)
+	{
+		_puterror("Usage: unsetenv VARIABLE\n");
+		return (-1);
+	}
+
+	n = arguments[1];
+
+	if (unsetenv(n) != 0)
+	{
+		_puterror("unsetenv");
+	}
+	return (0);
+}
