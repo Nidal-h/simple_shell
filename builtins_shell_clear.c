76239d977dec9e416c897908797ddf66eca345b6
Nidal-h <hadrou.nidal@gmail.com>
2023-11-13 13:12:12 -0800

    0x16. C - Simple Shell

---
+++ builtins_shell_clear.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * shell_clear : Resets the screen of terminal.
+ * @arguments: the array of arguments
+ * Return: 1 to proceed with execution,
+ *         0 to exit.
+*/
+int shell_clear(char **arguments)
+{
+	(void)arguments; 
+	_puts("\033[2J\033[H");
+	return (1);
+}
