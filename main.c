396e1f1cca250de266036502264f6619705604e1
Ammar <you@example.com>
2023-11-15 21:15:03 +0100

    Done

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 #include "general.h"
 #include "main.h"
 
-/**
- * main - Entry Point
- * @argc: number of arguments received
- * @argv: arguments received
- * Return: 0
- **/
 
 int main(int argc, char **argv)
 {
